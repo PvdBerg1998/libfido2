774bd9e15ad2ae09c6225fcbd74102c5adae1b4b
PvdBerg1998 <PvdBerg1998@users.noreply.github.com>
2019-06-29 21:16:13 +0200

    Add setters for Credential

--- src/credential.rs
+++ src/credential.rs
@@ -1,6 +1,10 @@
-use crate::ffi::NonNull;
+use crate::{ffi::NonNull, FidoError, Result, FIDO_OK};
+use bitflags::bitflags;
 use libfido2_sys::*;
-use std::{ffi::CStr, slice};
+use std::{error, ffi::CStr, fmt, os::raw, slice, str::FromStr};
+
+// @TODO: Create types for getters/setters instead of using byte slices
+// This is out of scope for now
 
 pub struct Credential {
     pub(crate) raw: NonNull<fido_cred>,
@@ -61,6 +65,131 @@ impl Credential {
             }
         }
     }
+
+    pub fn set_format(&mut self, fmt: CredentialFormat) -> Result<()> {
+        unsafe {
+            match fido_cred_set_fmt(self.raw.as_ptr_mut(), fmt.to_ffi()) {
+                FIDO_OK => Ok(()),
+                err => Err(FidoError(err)),
+            }
+        }
+    }
+
+    pub fn set_auth_data(&mut self, auth_data: &[u8]) -> Result<()> {
+        unsafe {
+            match fido_cred_set_authdata(
+                self.raw.as_ptr_mut(),
+                auth_data as *const _ as *const _,
+                auth_data.len(),
+            ) {
+                FIDO_OK => Ok(()),
+                err => Err(FidoError(err)),
+            }
+        }
+    }
+
+    pub fn set_client_data_hash(&mut self, client_data_hash: &[u8]) -> Result<()> {
+        unsafe {
+            match fido_cred_set_clientdata_hash(
+                self.raw.as_ptr_mut(),
+                client_data_hash as *const _ as *const _,
+                client_data_hash.len(),
+            ) {
+                FIDO_OK => Ok(()),
+                err => Err(FidoError(err)),
+            }
+        }
+    }
+
+    pub fn set_signature(&mut self, signature: &[u8]) -> Result<()> {
+        unsafe {
+            match fido_cred_set_sig(
+                self.raw.as_ptr_mut(),
+                signature as *const _ as *const _,
+                signature.len(),
+            ) {
+                FIDO_OK => Ok(()),
+                err => Err(FidoError(err)),
+            }
+        }
+    }
+
+    pub fn set_x509_certificate(&mut self, x509_certificate: &[u8]) -> Result<()> {
+        unsafe {
+            match fido_cred_set_x509(
+                self.raw.as_ptr_mut(),
+                x509_certificate as *const _ as *const _,
+                x509_certificate.len(),
+            ) {
+                FIDO_OK => Ok(()),
+                err => Err(FidoError(err)),
+            }
+        }
+    }
+
+    pub fn set_relying_party(&mut self, id: &CStr, name: &CStr) -> Result<()> {
+        unsafe {
+            match fido_cred_set_rp(self.raw.as_ptr_mut(), id.as_ptr(), name.as_ptr()) {
+                FIDO_OK => Ok(()),
+                err => Err(FidoError(err)),
+            }
+        }
+    }
+
+    pub fn set_user(
+        &mut self,
+        user_id: &[u8],
+        name: &CStr,
+        display_name: &CStr,
+        icon: &CStr,
+    ) -> Result<()> {
+        unsafe {
+            match fido_cred_set_user(
+                self.raw.as_ptr_mut(),
+                user_id as *const _ as *const _,
+                user_id.len(),
+                name.as_ptr(),
+                display_name.as_ptr(),
+                icon.as_ptr(),
+            ) {
+                FIDO_OK => Ok(()),
+                err => Err(FidoError(err)),
+            }
+        }
+    }
+
+    pub fn set_extensions(&mut self, extensions: CredentialExtensions) -> Result<()> {
+        unsafe {
+            match fido_cred_set_extensions(self.raw.as_ptr_mut(), extensions.bits()) {
+                FIDO_OK => Ok(()),
+                err => Err(FidoError(err)),
+            }
+        }
+    }
+
+    pub fn set_options(&mut self, options: CredentialOptions) -> Result<()> {
+        unsafe {
+            match fido_cred_set_options(
+                self.raw.as_ptr_mut(),
+                options.contains(CredentialOptions::RESIDENT_KEY),
+                options.contains(CredentialOptions::USER_VERIFICATION),
+            ) {
+                FIDO_OK => Ok(()),
+                err => Err(FidoError(err)),
+            }
+        }
+    }
+
+    /// # Remarks
+    /// - This method can only be called once and will return an error afterwards
+    pub fn set_type(&mut self, credential_type: CredentialType) -> Result<()> {
+        unsafe {
+            match fido_cred_set_type(self.raw.as_ptr_mut(), credential_type as raw::c_int) {
+                FIDO_OK => Ok(()),
+                err => Err(FidoError(err)),
+            }
+        }
+    }
 }
 
 // libfido2_sys guarantees this.
@@ -74,5 +203,69 @@ impl Drop for Credential {
             fido_cred_free(&mut credential as *mut _);
             assert!(credential.is_null());
         }
+    }
+}
+
+bitflags! {
+    pub struct CredentialExtensions: raw::c_int {
+        const HMAC_SECRET = FIDO_EXT_HMAC_SECRET as raw::c_int;
+    }
+}
+
+bitflags! {
+    pub struct CredentialOptions: u8 {
+        const RESIDENT_KEY = 1;
+        const USER_VERIFICATION = 2;
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum CredentialFormat {
+    Fido2,
+    FidoU2F,
+}
+
+impl CredentialFormat {
+    const FIDO2_FORMAT: &'static str = "packed";
+    const FIDO2_FORMAT_CSTR: *const raw::c_char = b"packed\0" as *const _ as *const _;
+    const FIDO_U2F_FORMAT: &'static str = "fido-u2f";
+    const FIDO_U2F_FORMAT_CSTR: *const raw::c_char = b"fido-u2f\0" as *const _ as *const _;
+
+    pub(crate) fn to_ffi(&self) -> *const raw::c_char {
+        match self {
+            CredentialFormat::Fido2 => Self::FIDO2_FORMAT_CSTR,
+            CredentialFormat::FidoU2F => Self::FIDO_U2F_FORMAT_CSTR,
+        }
+    }
+}
+
+impl FromStr for CredentialFormat {
+    type Err = InvalidCredentialFormatError;
+
+    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
+        match s {
+            CredentialFormat::FIDO2_FORMAT => Ok(CredentialFormat::Fido2),
+            CredentialFormat::FIDO_U2F_FORMAT => Ok(CredentialFormat::FidoU2F),
+            _ => Err(InvalidCredentialFormatError),
+        }
+    }
+}
+
+#[repr(i32)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum CredentialType {
+    RS256 = COSE_RS256,
+    ES256 = COSE_ES256,
+    //EDDSA = COSE_EDDSA
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub struct InvalidCredentialFormatError;
+
+impl error::Error for InvalidCredentialFormatError {}
+
+impl fmt::Display for InvalidCredentialFormatError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "The credential string is invalid")
     }
 }
